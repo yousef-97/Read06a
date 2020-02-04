d2abab9f96834cc369b737682c7b5d2e1c202114
Yousef Majdi <alshenyouef@gmail.com>
2020-02-04 20:54:45 +0200

    js-content-read06

---
+++ add-content.js
@@ -0,0 +1,9 @@
+var today =new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18){
+    greeting = 'good evening!';}
+else if (hourNow >12){greeting = 'good afternoon!';} 
+ else if(hourNow> 0){greeting = 'good morning!';}
+ else{greeting = 'Welcome!';}  
+ document.write('<h3>' + greeting + '</h3>')

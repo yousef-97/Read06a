20095c829a81973fb785700dec9a4ba226339273
Yousef Majdi <alshenyouef@gmail.com>
2020-02-05 13:41:52 +0200

    adding function

--- add-content.js
+++ add-content.js
@@ -1,9 +1,25 @@
-var today =new Date();
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 18){
-    greeting = 'good evening!';}
-else if (hourNow >12){greeting = 'good afternoon!';} 
- else if(hourNow> 0){greeting = 'good morning!';}
- else{greeting = 'Welcome!';}  
- document.write('<h3>' + greeting + '</h3>')
+// var today =new Date();
+// var hourNow = today.getHours();
+// var greeting;
+// if (hourNow > 18){
+//     greeting = 'good evening!';}
+// else if (hourNow >12){greeting = 'good afternoon!';} 
+//  else if(hourNow> 0){greeting = 'good morning!';}
+//  else{greeting = 'Welcome!';}  
+//  document.write('<h3>' + greeting + '</h3>')
+
+function theTime(){
+
+    // var today =new Date();
+    var hourNow = prompt("enter the time 0-24")
+    var greeting;
+    if (hourNow > 18 && hourNow<=24){
+        greeting = 'good evening!';}
+    else if (hourNow >12 && hourNow<=18){greeting = 'good afternoon!';} 
+     else if(hourNow> 0&& hourNow<=12){greeting = 'good morning!';}
+     else{greeting = 'Welcome!';} 
+     return greeting; 
+     }
+
+document.write('<h3>' + theTime() + '</h3>')
+     
